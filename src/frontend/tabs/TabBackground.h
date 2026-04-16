932e5b53c9462d623f939a495ca7cee0ec147f5b
PyMo <moritz.limbacher@gmail.com>
2026-04-16 14:24:22 +0200

    build: added -lfltk_images and -lpng to Windows build command for image support icon1 image added

--- src/frontend/tabs/TabBackground.h
+++ src/frontend/tabs/TabBackground.h
@@ -1,12 +1,23 @@
 #pragma once
 #include <FL/Fl_Group.H>
 #include <FL/Fl_Box.H>
+#include <FL/Fl_Choice.H>
+#include <FL/Fl_Button.H>
+
 class TabBackground : public Fl_Group {
 public:
     TabBackground(int X, int Y, int W, int H, const char* L = 0) : Fl_Group(X, Y, W, H, L) {
-        Fl_Box* box = new Fl_Box(X + 10, Y + 10, W - 20, H - 20, "${t}");
-        box->box(FL_ENGRAVED_BOX);
-        box->align(FL_ALIGN_CENTER | FL_ALIGN_INSIDE);
+        Fl_Box* lbl_method = new Fl_Box(X + 20, Y + 20, 80, 25, "Method:");
+        lbl_method->align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE);
+        
+        Fl_Choice* ch_method = new Fl_Choice(X + 100, Y + 20, 120, 25);
+        ch_method->add("single");
+        ch_method->add("multiple");
+        ch_method->value(0);
+
+        Fl_Button* btn_record = new Fl_Button(X + 20, Y + 60, 100, 30, "Record BG");
+        Fl_Button* btn_save = new Fl_Button(X + 130, Y + 60, 100, 30, "Save BG");
+
         end();
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 #include <FL/Fl.H>
+#include <FL/Fl_PNG_Image.H>
 #include "frontend/FrontendConnection.h"
 
 int main(int argc, char** argv) {
     Fl::scheme("gtk+"); // Make the GUI look more modern
+    
+    // Load and set the application icon
+    Fl_RGB_Image* icon = new Fl_PNG_Image("icon1.png"); // if run from same directory
+    if (icon->fail()) {
+        delete icon;
+        icon = new Fl_PNG_Image("build/graphics/icon1.png"); // if run from project root
+    }
+    
+    if (!icon->fail()) {
+        Fl_Window::default_icon(icon);
+    } else {
+        delete icon; // Fallback: no custom image will be loaded
+    }
+    
     FrontendConnection* window = new FrontendConnection(1200, 800, "PLEM Multi-Pane Frontend");
     window->show(argc, argv);
     return Fl::run();
